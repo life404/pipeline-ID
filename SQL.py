1be3a5b8bdf3e8d322f7bb6142a15eff7f52fb0e
Geng Yang <32447916+life404@users.noreply.github.com>
2021-02-20 22:20:18 +0800

    add new feature

--- SQL.py
+++ SQL.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 
-import sqlite3
-import os
-from Search import *
 import collections
+import os
+import sqlite3
+
 from icecream import ic
+
+from Search import *
+
 
 class SQL():
     def __init__(self, Search, dbname):
@@ -77,6 +80,8 @@ class SQL():
 
     def Tree(self):
         self.Init()
+        self.level = 0
+        self.column = "Uuid, Parent, Name"
         if self.Search.uuid != False or self.Search.name != False:
             select = ("SELECT Uuid, Parent FROM PPID_CONTENT WHERE Uuid='%s' OR Name='%s'" %(self.Search.uuid, self.Search.name))
             self.cursor.execute(select)
@@ -85,8 +90,10 @@ class SQL():
             if select_node:
                 parent_node_list = collections.OrderedDict()
                 child_node_list = collections.OrderedDict()
+#                parent_node_list[0] = select_node[0]
+#                child_node_list[0] = select_node[0]
                 select_child = select_node[0]
-                select_parent = select_node[1]
+                select_parent = select_node[-1]
 
                 if select_node[1] == "...":
                     child_level = 0
@@ -126,17 +133,42 @@ class SQL():
                             select_parent = parent_node[1]
                             parent_tag = True
                         else:
-                            parent_tag = False         
+                            parent_tag = False  
+
+                tree_node = collections.OrderedDict()
+                tree_num = 1
+                for key in list(parent_node_list.keys())[::-1]:
+                    tree_node[tree_num] = parent_node_list[key]
+                    tree_num += 1
+                tree_node[tree_num] = select_node[0]
+                tree_num += 1
+                for key in child_node_list.keys():
+                    tree_node[tree_num] = child_node_list[key]
+
+                print(".")
+                for key in list(tree_node.keys())[:-1]:
+                    print('├──', end=" ")
+                    self.cursor.execute("SELECT %s FROM PPID_CONTENT WHERE Uuid='%s'" %(self.column, tree_node[key]))
+                    content = self.cursor.fetchone()
+                    print(content[0])
+                    for i in content[1:]:
+                        print("    %s" %(i))
+                print("└──", end=" ")
+                self.cursor.execute("SELECT %s FROM PPID_CONTENT WHERE Uuid='%s'" %(self.column, tree_node[list(tree_node)[-1]]))
+                content = self.cursor.fetchone()
+                print(content[0])
+                for i in content[1:]:
+                    print("    %s" %(i))
+                    
+
             else:
                 print("Can't find record about your input")
 
-            
-            if parent_node_list == collections.OrderedDict():
-                pass
-
         self.Close()
-
-
-
-
     
+        
+
+
+
+
+            
